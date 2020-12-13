f7b8769cf25a79deb8abb6f2fed92f844626d13d
Christian <heussy@gmail.com>
2020-12-13 09:27:25 -0800

    Added binary tree

---
+++ binary_tree/main.cpp
@@ -0,0 +1,17 @@
+#include "binary_tree.h"
+#include <iostream>
+
+int main()
+{
+    binary_tree my_tree = binary_tree(5);
+    my_tree.insert(3);
+    my_tree.insert(2);
+    my_tree.insert(4);
+    my_tree.insert(10);
+    my_tree.insert(9);
+    my_tree.insert(11);
+
+    my_tree.print_tree();
+    
+    return 0;
+}
